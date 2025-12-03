37d6e8f49dc2e0e8d2d32e3962245d7b51f46b60
Mohsin Javeed <mohsinjaveed662@gmail.com>
2025-12-03 12:34:42 +0500

    Specify Chrome executable path

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const SCORES_FILE = "./scores.json";
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+    puppeteer: {
+        // 👇 This line fixes the "Could not find browser" error
+        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
 });
-
 client.on('qr', (qr) => qrcode.generate(qr, { small: true }));
 
 client.on('ready', () => {
